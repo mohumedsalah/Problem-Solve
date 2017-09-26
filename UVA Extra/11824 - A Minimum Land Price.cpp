808a37303752ae8cfc11c84d0584056f4faea0e2
Unknown <mohumed.s.omar@gmail.com>
2017-09-26 02:13:17 +0200

    1

---
+++ UVA Extra/11824 - A Minimum Land Price.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define repi(i, j, n) 		for(int i=(j);i<(int)(n);++i)
+#define rln() getchar()
+#define rint(n) scanf("%d",&n)
+#define rs(n) scanf("%s",n)
+#define rc(n) scanf("%c",&n)
+#define rf(n) scanf("%lf",&n)
+#define rl(n) scanf("%lld",&n)
+#define pint(x) printf("%d", x)
+#define psp() printf(" ")
+#define ps(x) printf("%s", x)
+#define pc(x) printf("%c", x)
+#define pnl() printf("\n")
+#define pl(x) printf("%lld", x)
+#define pf(x) printf("%.6lf", x)
+#define isOn(S, j) (S & (1 << j))
+#define setBit(S, j) (S |= (1 << j))
+#define clearBit(S, j) (S &= ~(1 << j))
+#define toggleBit(S, j) (S ^= (1 << j))
+#define lowBit(S) (S & (-S))
+#define setAll(S, n) (S = (1 << n) - 1)
+#define sz(v) ((int)((v).size()))
+#define ssz(s) ((int)strlen(s))
+#define f first
+#define s second
+#define INF_MAX 2147483647
+#define INF_MIN -2147483647
+#define INF_LL 2000000000000000000LL
+#define INF 1000000000
+#define EPS 1e-8
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+vi arr;
+int main(){
+	//freopen("in.txt","r", stdin);
+	int t,xx;
+	rint(t);
+	while(t--){
+		while(rint(xx) && xx){
+			arr.pb(xx);
+		}
+		sort(arr.begin(), arr.end());
+		reverse(arr.begin(), arr.end());
+		int ans = 0, fl = 1;
+		repi(i,0, sz(arr)){
+			ans += (pow(arr[i],(i + 1)) * 2);
+			if(ans > 5000000){
+				fl = 0;
+				break;
+			}
+
+		}
+		if(fl)
+			pint(ans), pnl();
+		else
+			puts("Too expensive");
+		arr.clear();
+	}
+
+
+	return 0;
+}
